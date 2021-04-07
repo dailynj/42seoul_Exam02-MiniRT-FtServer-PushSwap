cc7e642eb1c0acfc964d92b3ab1cedd8940df64d
najeong12 <leena9901@gmail.com>
2021-04-07 18:12:29 +0900

    add minirt.c

--- miniRT/minirt.c
+++ miniRT/minirt.c
@@ -10,15 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <mlx.h>
 #include "mlx/mlx.h"
+#include <unistd.h>
+
+typedef struct s_data
+{
+	void 	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
+
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int main()
 {
 	void	*mlx_ptr;
-	void	*win_ptr; // 생성할 윈도우를 가리키는 포인터
-	
-	mlx_ptr = mlx_init();	
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hellow World!"); 
-	mlx_loop(mlx_ptr); // 생성한 윈도우가 있는 mlx_ptr을 rendering한다. 
+	void	*win_ptr;
+	t_data	image;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Hellow World!");
+	image.img = mlx_new_image(mlx_ptr, 1920, 1080); // 이미지 객체 생성
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian); // 이미지 주소 할당
+	for (int i = 0 ; i < 1000 ; i++)
+	{
+		//mlx_pixel_put (mlx_ptr, win_ptr, i, i, 0x00FF0000);
+		my_mlx_pixel_put(&image, i, i, 0x00FF0000);
+	}
+	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
+	mlx_loop(mlx_ptr);
 	return (0);
 }
