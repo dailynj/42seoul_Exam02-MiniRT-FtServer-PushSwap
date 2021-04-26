24561b278b9c48b20b39944dc24a038e11cb0103
najeong12 <leena9901@gmail.com>
2021-04-26 17:57:16 +0900

    finish 0426

--- 0426 new miniRT/color.c
+++ 0426 new miniRT/color.c
@@ -19,11 +19,27 @@ int write_color(int t, t_color pixel_color)
 // Image의 크기 및 종횡비, Projection 공간에서 나타나는 Vector에 따른 색상을 결정
 t_vec   	ray_color(t_ray r)
 {
+	t_vec color;
+	t_vec center = point3(0, 0, -1);
     // 빨간 구 그리기
-    if (hit_sphere(point3(0,0,-1), 0.5, r)) // 추가된 부분!!
-        return color(1, 0, 0);              // 추가된 부분!!
+    // color = vec(1, 0, 0);
+	// if (hit_sphere(center, 0.5, r))
+	// 	return (color);
+	
+	// 그라데이션 구 그리기
+	
+	double t = hit_sphere(center, 0.5, r);
+	if (t > 0.0) // 만약 해가 있다면!
+	{
+		t_vec tmp = ray_at(&r, t); // t를 활용해서 tmp 벡터를 구한다.
+		t_vec N = vec_unit(vec(tmp.x - 0, tmp.y - 0, tmp.z - (-1))); // tmp 벡터의 unit벡터를 구한다! 
+		color = vec_mul_num(0.5, vec(N.x + 1, N.y + 1, N.z + 1));
+		return (color);
+	}  
 
-	t_vec unit_direction = vec_unit(r.dir);
-    double t = 0.5 * (unit_direction.y + 1.0);
-    return vec_add(vec_mul_num(1.0 - t, color(1.0, 1.0, 1.0)), vec_mul_num(t, color(0.5, 0.7, 1.0)));
+	// 해가 없다면~!
+	t_vec unit_direction = vec_unit(r.dir);   
+	t = 0.5 * (unit_direction.y + 1.0);
+	color = vec(1.0 - 0.5 * t, 1.0 - 0.3 * t, 1.0);
+  return (color);
 }
--- 0426 new miniRT/sphere.c
+++ 0426 new miniRT/sphere.c
@@ -6,7 +6,8 @@ int hit_sphere(t_vec center, double radius, t_ray r) {
     double b = 2.0 * vec_dot(oc, r.dir);
     double c = vec_dot(oc, oc) - radius * radius;
     double discriminant = b * b - 4 * a * c;
-    if (discriminant > 0)
-            return (1);
-    return (0);
+    if (discriminant < 0)
+		return (-1);
+	else
+		return ((-b - sqrt(discriminant) / (2.0 * a)));
 }
